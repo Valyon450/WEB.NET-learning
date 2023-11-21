7702454aded0ab3720c6871f73d87cb7ed813c1e
Valyon450 <valentinbasuk@gmail.com>
2023-11-21 02:35:26 +0200

    feat(Lab1): add project files

---
+++ Lab1/ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyCollectionLibrary;
+
+var stack = new MyStack<int>();
+
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Peek());
+
+Console.WriteLine("Stack contents:");
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
---
+++ Lab1/MyCollectionLibrary/MyStack.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace MyCollectionLibrary;
+
+public class MyStack<T> : IEnumerable<T>
+{
+    private class Item
+    {
+        public T? Data { get; set; }
+        public Item? Next { get; set; }
+    }
+
+    private Item? _top;
+
+    public void Push(T item)
+    {
+        var newItem = new Item { Data = item, Next = _top };
+        _top = newItem;        
+    }
+
+    public T Pop()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Stack is empty.");
+
+        T data = _top.Data;
+        _top = _top.Next;        
+        return data;
+    }
+
+    public T Peek()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Stack is empty.");
+
+        return _top.Data;
+    }
+
+    public bool IsEmpty()
+    {
+        return _top == null;
+    }
+
+    public void Clear()
+    {
+        _top = null;        
+    }
+
+    public bool Contains(T item)
+    {
+        Item? current = _top;
+        while (current != null)
+        {
+            if (EqualityComparer<T>.Default.Equals(current.Data, item))
+                return true;
+
+            current = current.Next;
+        }
+
+        return false;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        foreach (var item in this)
+        {
+            sb.AppendLine(item?.ToString());
+        }
+        return sb.ToString();
+    }
+
+    // Implementation of the IEnumerable<T> interface
+    public IEnumerator<T> GetEnumerator()
+    {
+        return new MyEnumerator(this);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    // Implementation of the own IEnumerator<T> interface
+    private class MyEnumerator : IEnumerator<T>
+    {
+        private Item? _current;
+        private readonly MyStack<T> _stack;
+
+        public MyEnumerator(MyStack<T> stack)
+        {
+            _stack = stack;
+            _current = null;
+        }
+
+        public bool IsHasNext()
+        {
+            return _current != null;
+        }
+
+        public T Current
+        {
+            get
+            {
+                if (!IsHasNext())
+                    throw new InvalidOperationException("Enumerator is not started.");
+
+                return _current.Data;
+            }
+        }
+
+        object IEnumerator.Current => _current.Data;
+
+        public void Dispose()
+        {
+            // Don't need to perform additional actions when freeing resources.
+        }
+
+        public bool MoveNext()
+        {
+            if (!IsHasNext())
+                _current = _stack._top;
+            else
+                _current = _current.Next;
+
+            return IsHasNext();
+        }
+
+        public void Reset()
+        {
+            _current = null;
+        }
+    }
+}
