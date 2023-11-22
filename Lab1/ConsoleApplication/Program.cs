ee7824ee47a3cd46c2d827f336c53bd26ed944e9
Valyon450 <valentinbasuk@gmail.com>
2023-11-22 09:45:59 +0200

    feat(Lab1): add some demonstration for ConsoleApp

--- Lab1/ConsoleApplication/Program.cs
+++ Lab1/ConsoleApplication/Program.cs
@@ -9,11 +9,32 @@ stack.OnClear += (sender, item) => Console.WriteLine($"Stack cleared");
 stack.Push(1);
 stack.Push(2);
 stack.Push(3);
-Console.WriteLine(stack.Pop());
-Console.WriteLine(stack.Peek());
+Console.WriteLine($"Pop: {stack.Pop()}");
+Console.WriteLine($"Peek: {stack.Peek()}");
+
+bool isContains = stack.Contains(2);
+if (isContains)
+    Console.WriteLine("Stack contains 2");
+else
+    Console.WriteLine("Item 2 is not in the collection");
+
+isContains = stack.Contains(3);
+if (isContains)
+    Console.WriteLine("Stack contains 3");
+else
+    Console.WriteLine("Item 3 is not in the collection");
+
+Console.WriteLine("Stack contents:");
+foreach (var item in stack)
+{
+    Console.WriteLine($"Item: {item}");
+}
+
+stack.Clear();
 
 Console.WriteLine("Stack contents:");
 foreach (var item in stack)
 {
     Console.WriteLine(item);
 }
+
