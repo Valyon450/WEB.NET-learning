3f44cc1153f22940b64520288836e5c7d3b0209c
Valyon450 <valentinbasuk@gmail.com>
2023-11-26 23:47:20 +0200

    feat(Lab2): add Enumerator tests

--- Lab2/CollectionTests/StackTests.cs
+++ Lab2/CollectionTests/StackTests.cs
@@ -106,7 +106,7 @@ namespace CollectionTests
             [Fact]
             public void Should_Return_True_When_Stack_Empty()
             {
-                var stack = new MyStack<int>();                
+                var stack = new MyStack<int>();
 
                 Assert.True(stack.IsEmpty());
             }
@@ -122,7 +122,7 @@ namespace CollectionTests
                 Assert.False(stack.IsEmpty());
             }
         }
- 
+
         public class ClearTests
         {
             [Fact]
@@ -193,7 +193,7 @@ namespace CollectionTests
             {
                 var stack = new MyStack<int>();
                 stack.Push(1);
-                stack.Push(2);                
+                stack.Push(2);
 
                 var stackToString = stack.ToString();
 
@@ -214,10 +214,135 @@ namespace CollectionTests
                 stack.Push(3);
 
                 var stackToString = stack.ToString();
-                
-                Assert.Equal("3\r\n2\r\n1\r\n", stackToString);                
+
+                Assert.Equal("3\r\n2\r\n1\r\n", stackToString);
             }
         }
-        
+
+        public class GetEnumeratorTests
+        {
+            [Fact]
+            public void Should_Return_Generic_Enumerator()
+            {
+                var stack = new MyStack<int>();
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+
+                IEnumerator<int> enumerator = stack.GetEnumerator();
+
+                Assert.NotNull(enumerator);
+            }
+
+            [Fact]
+            public void Should_Return_Non_Generic_Enumerator()
+            {
+                var stack = new MyStack<int>();
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+
+                IEnumerator enumerator = stack.GetEnumerator();
+
+                Assert.NotNull(enumerator);
+            }
+
+            [Fact]
+            public void Should_Traverse_Stack_Items()
+            {
+                var stack = new MyStack<int>();
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+
+                List<int> items = new List<int>();
+                foreach (int item in stack)
+                {
+                    items.Add(item);
+                }
+
+                Assert.Equal(new List<int> { 3, 2, 1 }, items);
+            }
+
+        }
+
+        public class MyEnumeratorTests
+        {
+            [Fact]
+            public void Should_Return_Current_Element()
+            {
+                var stack = new MyStack<int>();
+                stack.Push(1);
+                stack.Push(2);
+                var enumerator = stack.GetEnumerator();
+                enumerator.MoveNext();
+                var current = enumerator.Current;
+
+                Assert.Equal(2, current);
+            }
+
+            [Fact]
+            public void Should_Return_Current_Element_Non_Generic_Enumerator()
+            {
+                var stack = new MyStack<int>();
+                stack.Push(1);
+                stack.Push(2);
+                IEnumerator enumerator = stack.GetEnumerator();
+                enumerator.MoveNext();
+                var current = enumerator.Current;
+
+                Assert.Equal(2, current);
+            }
+
+            [Fact]
+            public void Current_Should_Return_Exeption()
+            {
+                var stack = new MyStack<int>();
+
+                IEnumerator<int> enumerator = stack.GetEnumerator();
+
+                Assert.Throws<InvalidOperationException>(() => enumerator.Current);
+            }
+
+            [Fact]
+            public void Should_Move_To_Next_Element()
+            {
+                var stack = new MyStack<int>();
+
+                stack.Push(1);
+                stack.Push(2);
+
+                var enumerator = stack.GetEnumerator();
+
+                var result1 = enumerator.MoveNext();
+                var result2 = enumerator.MoveNext();
+                var result3 = enumerator.MoveNext();
+
+                Assert.True(result1);
+                Assert.True(result2);
+                Assert.False(result3);
+            }
+
+            [Fact]
+            public void Should_Reset_Enumerator()
+            {
+                var stack = new MyStack<int>();
+
+                stack.Push(1);
+                stack.Push(2);
+
+                var enumerator = stack.GetEnumerator();
+
+                enumerator.MoveNext();
+
+                var current = enumerator.Current;
+
+                enumerator.Reset();
+
+                enumerator.MoveNext();
+
+                Assert.Equal(current, enumerator.Current);
+            }
+        }
     }
 }
